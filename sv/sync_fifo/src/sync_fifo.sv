1383c00114c5cbc0dedcfa1fa2a9cf1138fbffd8
Tim H <Heaton15@gmail.com>
2024-12-18 12:42:57 -0500

    Some updates to simulate right in verilator   1. 1 => needs to be 1'b1 to be incremented correctly   2. Recall that logic = is not allowed (but wire = is)   3. Updated the size of the pointers so that the FIFO can fill up      completely.

--- sv/sync_fifo/src/sync_fifo.sv
+++ sv/sync_fifo/src/sync_fifo.sv
@@ -35,26 +35,30 @@ module sync_fifo (  /*AUTOARG*/
   output full, empty;
 
   // Points to the address in the queue
-  logic [$clog2(FIFO_DEPTH)-1:0] wptr, rptr;
+  logic [$clog2(FIFO_DEPTH):0] wptr, rptr;
 
   // Fits 8, 64 bit words
   logic [DATA_WIDTH-1:0] queue[FIFO_DEPTH];
+  logic can_write;
+  logic can_read;
 
-  logic can_write = wr_en & !full;
-  logic can_read = rd_en & !empty;
+
 
 
   // Empty when the pointers are equal
   assign empty = wptr == rptr;
-  assign full  = (wptr + 1) == rptr;  // Should wrap back around to rptr value
+  assign full = (wptr[$clog2(FIFO_DEPTH)] ^ rptr[$clog2(FIFO_DEPTH)]) & (wptr[2:0] == rptr[2:0]);
+
+  assign can_write = wr_en & !full;
+  assign can_read = rd_en & !empty;
 
   always_ff @(posedge clock or negedge rst) begin
     if (!rst) begin
       wptr <= 0;
     end else begin
       if (can_write) begin
-        queue[wptr] <= din;
-        wptr <= wptr + 1;
+        queue[wptr[2:0]] <= din;
+        wptr <= wptr + 1'b1;
       end
     end
   end
@@ -64,8 +68,8 @@ module sync_fifo (  /*AUTOARG*/
       rptr <= 0;
     end else begin
       if (can_read) begin
-        dout <= queue[rptr];
-        rptr <= rptr + 1;
+        dout <= queue[rptr[2:0]];
+        rptr <= rptr + 1'b1;
       end
     end
   end
@@ -73,6 +77,59 @@ module sync_fifo (  /*AUTOARG*/
 endmodule
 
 module tb;
+  parameter int DATA_WIDTH = 64;
+
+  logic clock;
+  logic rst;
+
+  initial begin
+    clock = 0;
+    #1;
+    rst = 0;
+    forever begin
+      clock = #1 !clock;
+    end
+  end
+  initial dump("sync_fifo.vcd");
+
+  initial begin
+    #3;
+    rst = 1;
+  end
+
+  logic [DATA_WIDTH-1:0] dout;
+  logic                  empty;
+  logic                  full;
+  logic [DATA_WIDTH-1:0] din;
+  logic                  rd_en;
+  logic                  wr_en;
+
+  initial din = 64'hFFFF;
+
+  initial begin
+    wr_en = 0;
+    rd_en = 0;
+
+    repeat (5) @(posedge clock);
+    wr_en = 1;
+    repeat (20) @(posedge clock);
+    $finish;
+  end
+
+
+
+  sync_fifo #(  /*AUTOINSTPARAMS*/) dut (  /*AUTOINST*/
+      // Outputs
+      .dout (dout[DATA_WIDTH-1:0]),
+      .full (full),
+      .empty(empty),
+      // Inputs
+      .clock(clock),
+      .rst  (rst),
+      .din  (din[DATA_WIDTH-1:0]),
+      .wr_en(wr_en),
+      .rd_en(rd_en)
+  );
 
 endmodule
 
