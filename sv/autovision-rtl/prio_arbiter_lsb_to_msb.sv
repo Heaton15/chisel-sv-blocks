bd30f34d8522ae889eeab19dcfdd17891a1ffcbe
Tim <Heaton15@gmail.com>
2025-09-04 12:42:09 -0400

    Add the fast form of the LSB prio arbiter

--- sv/autovision-rtl/prio_arbiter_lsb_to_msb.sv
+++ sv/autovision-rtl/prio_arbiter_lsb_to_msb.sv
@@ -1,26 +1,31 @@
 /* The LSB has the highest priority*/
-module prio_arbiter_lsb_to_msb (  /*AUTOARG*/
+module prio_arbiter_lsb_to_msb #(
+    parameter int SIZE = 4
+
+) (  /*AUTOARG*/
     // Outputs
-    gnt,
+    output [SIZE-1:0] gnt,
     // Inputs
-    req
+    input  [SIZE-1:0] req
 );
 
-  parameter int SIZE = 4;
-
-  input [SIZE-1:0] req;
-  output [SIZE-1:0] gnt;
-
+`ifdef comb_arbiter
   logic [SIZE-1:0] higher_prio;
 
   // The trick here is that the moment a higher_prio bit gets set, all bits above it
   // will become set and will negate to 0 in the gnt assignment
+  /* verilator lint_off ALWCOMBORDER */
   always_comb begin
     higher_prio[0] = 1'b0;
     for (int i = 1; i < SIZE; i++) begin
       higher_prio[i] = higher_prio[i-1] | req[i-1];
     end
   end
+  /* verilator lint_on ALWCOMBORDER */
 
   assign gnt[SIZE-1:0] = req[SIZE-1:0] & ~higher_prio[SIZE-1:0];
+`else
+
+  assign gnt = req & (~req + 1'b1);
+`endif
 endmodule
