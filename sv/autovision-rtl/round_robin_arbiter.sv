a0a9ced2b5dbf451743e4254531cb15a938ce8ee
Tim <Heaton15@gmail.com>
2025-09-04 22:35:23 -0400

    Create the standard round robin arbiter with no control.
    Changes the standard pointer every clock cycle

--- sv/autovision-rtl/round_robin_arbiter.sv
+++ sv/autovision-rtl/round_robin_arbiter.sv
@@ -1,14 +1,47 @@
 `timescale 1ns / 1ns
-module round_robin_arbiter (  /*AUTOARG*/
-    // Outputs
-    gnt,
-    // Inputs
-    req
-);
-  parameter int SIZE = 4;
 
-  input [SIZE-1:0] req;
-  output [SIZE-1:0] gnt;
+// Conventional Round Robin Arbiter
+module round_robin_arbiter #(
+
+    parameter int SIZE = 4
+) (
+
+    // Outputs
+    output [SIZE-1:0] gnt,
+
+    // Inputs
+    input [SIZE-1:0] req,
+    input clk,
+    input rst
+);
+
+  logic [$clog2(SIZE)-1:0] prio_ptr;
+
+
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      prio_ptr <= 0;
+    end else begin
+      prio_ptr <= prio_ptr + 1'b1;
+    end
+  end
+
+  logic [1:0][SIZE-1:0] req_tmp;
+  logic [SIZE-1:0] gnt_tmp;
+
+  // Rotate 1
+  assign {req_tmp[1], req_tmp[0]} = {2{req}} >> prio_ptr;
+
+  // Priority Arbiter
+  wire [SIZE-1:0] tmp = req_tmp[0] & (~req_tmp[0] + 1'b1);
+
+  assign {gnt, gnt_tmp} = {2{tmp}} << prio_ptr;
+
+
+
+
+
+
 
 
 endmodule
--- sv/autovision-rtl/test_blocks.py
+++ sv/autovision-rtl/test_blocks.py
@@ -7,9 +7,11 @@ import cocotb
 import logging
 import random
 import argparse
-from cocotb.triggers import Timer
+from cocotb.handle import Immediate
+from cocotb.triggers import RisingEdge, Timer
 from cocotb_tools.runner import get_runner
 from dataclasses import dataclass
+from BitVector import BitVector
 
 
 @dataclass
@@ -31,6 +33,7 @@ def testbench(top_module: str):
     proj_dir = Path(__file__).parent
     sources = [proj_dir / "prio_arbiter_lsb_to_msb.sv"]
     sources.append(proj_dir / "prio_arbiter_msb_to_lsb.sv")
+    sources.append(proj_dir / "round_robin_arbiter.sv")
     defines = {}
 
     runner = get_runner(sim)
@@ -76,6 +79,46 @@ async def test_prio_arbiter_msb_to_lsb(dut):
             f"req: {rand_req:04b} : expected: {bin(expected)} : actual {dut.gnt.value}"
         )
         assert int(dut.gnt.value) == expected
+
+
+async def clock(dut):
+    dut.clk.value = 0
+    while True:
+        await Timer(1)
+        dut.clk.value = not dut.clk.value
+
+
+def compute_prio(curr_prio: int) -> int:
+    if curr_prio == 3:
+        return 0
+    else:
+        return curr_prio + 1
+
+
+def expected_rr_output(prio: int, req: int, size: int) -> int:
+    bv = BitVector(size=4, intVal=int(req))
+    bv >> prio
+    bv = BitVector(size=4, intVal=bv.int_val() & (~bv.int_val() + 1))
+    bv << prio
+    return bv.int_val()
+
+
+@cocotb.test()
+async def test_round_robin_arbiter(dut):
+    dut.rst.value = 1
+    logger = logging.getLogger("test")
+    cocotb.start_soon(clock(dut))
+    await RisingEdge(dut.clk)
+    dut.rst.value = 0
+
+    prio = 0
+    for _ in range(100000):
+        await RisingEdge(dut.clk)
+        rand_req = random.randrange(1, (1 << 4))
+        dut.req.value = rand_req
+        logger.info(f"req: {dut.req.value} gnt: {dut.gnt.value}, prio: {dut.prio_ptr_o.value}")
+        assert dut.gnt.value == expected_rr_output(prio, dut.req.value, 4)
+        prio = compute_prio(prio)
 
 
 if __name__ == "__main__":
